eb734e3c817ba2c78a72edb710fe1fa7010a519f
johlcar <johlcar2@gmail.com>
2019-05-29 10:16:51 -0500

    removed specific quotes

--- script.js
+++ script.js
@@ -123,16 +123,11 @@ function shortcutListener(e) {
 }
 
 function setupQuote() {
-  const quotes = ['And a step backward, after making a wrong turn, is a step in the right direction. <br/>- Kurt Vonnegut', 'Positivity, like any skill, you get better at by practicing it. <br/>- Jackson Bliton',
-    'All we have to decide is what to do with the time that is given us. <br/>- J.R.R. Tolkien', 'More is lost by indecision than wrong decision. <br/>- Cicero', "A ship is safe in harbor, but that's not what ships are for. <br/>- William Shedd",
-    'We are what we repeatedly do. Excellence, then, is not an act, but a habit. <br/>- Anonymous', 'Not all those who wander are lost. <br/>- J.R.R. Tolkien', 'Calm seas make poor sailors. <br/>- Anonymous',
-    'There is nothing noble in being superior to your fellow men, true nobility is being superior to your former self. <br/>- Ernest Hemingway',
-    'Everyone you meet is fighting a battle that you know nothing about. <br/>- Anonymous', 'Kindness is the language which the deaf can hear and the blind can see. <br/>- Mark Twain',
-    'To strive, to seek, to find, and not to yield. <br/>- Tennyson', 'Be the change you wish to see in the world. <br/>- Ghandi', 'Do or do not. There is no try. <br/>- Yoda',
-    'Would I rather be feared or loved? Easy - both. I want people to be afraid of how much they love me. <br/>- Michael Scott', 'Hodor. <br/>- Hodor',
-    "Don't ask for a lighter load, ask for a stronger back. <br/>- Phillips Brooks", 'Good judgement comes from experience and experience comes from bad judgement. <br/>- Anonymous',
-    'Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world. <br/>- Albert Einstein', 'Don’t count the days; make the days count. <br/>- Muhammad Ali', 'It isn’t the mountains ahead to climb that wear you out; it’s the pebble in your shoe. <br/>- Muhammad Ali',
+  const quotes = [
+    'Not all those who wander are lost. <br/>- J.R.R. Tolkien', 'Kindness is the language which the deaf can hear and the blind can see. <br/>- Mark Twain',
+     'Be the change you wish to see in the world. <br/>- Ghandi', 'Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world. <br/>- Albert Einstein', 'Don’t count the days; make the days count. <br/>- Muhammad Ali', 'It isn’t the mountains ahead to climb that wear you out; it’s the pebble in your shoe. <br/>- Muhammad Ali',
     "In three words I can sum up everything I've learned about life: it goes on. <br/>- Robert Frost", 'The man who moves a mountain begins by carrying away small stones. <br/>- Confucius', "You're only given a little spark of madness. You mustn't lose it. <br/>- Robin Williams", 'No matter what people tell you, words and ideas can change the world. <br/>- Robin Williams']
+
 
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
@@ -147,3 +142,10 @@ function main() {
 }
 
 main();
+
+const unusedQuotes = ['And a step backward, after making a wrong turn, is a step in the right direction. <br/>- Kurt Vonnegut', 'Positivity, like any skill, you get better at by practicing it. <br/>- Jackson Bliton', 'All we have to decide is what to do with the time that is given us. <br/>- J.R.R. Tolkien', 'More is lost by indecision than wrong decision. <br/>- Cicero', "A ship is safe in harbor, but that's not what ships are for. <br/>- William Shedd",
+    'We are what we repeatedly do. Excellence, then, is not an act, but a habit. <br/>- Anonymous', 'Calm seas make poor sailors. <br/>- Anonymous',
+    'There is nothing noble in being superior to your fellow men, true nobility is being superior to your former self. <br/>- Ernest Hemingway',
+    'Everyone you meet is fighting a battle that you know nothing about. <br/>- Anonymous', 'To strive, to seek, to find, and not to yield. <br/>- Tennyson', 'Do or do not. There is no try. <br/>- Yoda',
+    'Would I rather be feared or loved? Easy - both. I want people to be afraid of how much they love me. <br/>- Michael Scott', 'Hodor. <br/>- Hodor',
+    "Don't ask for a lighter load, ask for a stronger back. <br/>- Phillips Brooks", 'Good judgement comes from experience and experience comes from bad judgement. <br/>- Anonymous']
